168febbe2bfb845eecff40da5f95dddc377fa38d
Vinoth Kumar <vinoth.mani2@wipro.com>
2018-09-25 17:29:59 +0530

    Comparision operators in mongodb

--- index.js
+++ index.js
@@ -26,8 +26,18 @@ mongoose.connect('mongodb://localhost/playground')
     //const result = await nodeCourse.save();
     //console.log(result);
 
-    let courses = await Course.find();
-    console.log(courses);
+    const courses = await Course.find();
+    // console.log(courses);
+
+    const courses1 = await Course
+        // .find({ author: 'Vinoth' })
+        // .find({ price: { $gt :10 , $lt: 20 }})
+        // .find( { price : {$in : [10,20,30]}})
+        .limit(10)
+        .sort({ name: 1 })
+        .select({ name: 1 }) // Just output name field - don't get everything
+
+    console.log(courses1);
 })();
 
 
