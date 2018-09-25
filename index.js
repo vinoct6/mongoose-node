3d50aec178acbe518782106e51cdc0f7c6d93cee
Vinoth Kumar <vinoth.mani2@wipro.com>
2018-09-25 17:07:45 +0530

    Define a schema, create a model and save a document to mongo db

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/playground')
-        .then(() => console.log('connected to mongodb'))
-        .catch((err) =>console.log("Could not connect to mongodb"));
+    .then(() => console.log('connected to mongodb'))
+    .catch((err) => console.log("Could not connect to mongodb"));
+
+ /*jshint ignore:start*/   
+ (async () => {
+    const courseSchema = {
+        name: String,
+        author: String,
+        tags: [String],
+        date: { type: Date, default: Date.now },
+        isPublished: Boolean
+    };
+    
+    const Course = mongoose.model("courses", courseSchema);
+    
+    const nodeCourse = new Course({
+        name: 'Angular.js',
+        author: 'Vinoth',
+        tags: ['node', 'backend'],
+        isPublished: false
+    }); 
+    
+    const result = await nodeCourse.save();
+    console.log(result);
+ })();   
+
+
